a1137aaa84b2e18b1aa3101615ec092a8aaf3e79
Daria Rekhlova <dolly1993@yandex.ru>
2023-03-19 19:08:18 +0400

    добавили функцию find

--- model.py
+++ model.py
@@ -4,3 +4,19 @@ def read_phonebook():
         s = file.readlines()
         list = []
         return list
+   
+def find():
+    surname = input('Введите фамилию контакта: ')
+    print('В справочнике найдено: ')
+    with open('file.txt', 'r+', encoding= 'utf-8') as file:
+        s = file.readlines()
+    count = 0
+    for i in s:
+        if surname in i:
+            print(i, sep="\n")
+            count += 1
+    if count == 0:
+        print('К сожалению, контакт не найден')
+        return
+    
+    
